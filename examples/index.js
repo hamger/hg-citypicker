a4218bf7e4bc958bda0b1fcca101de345a037cd6
hamger <2313303800@qq.com>
2019-05-10 17:46:08 +0800

    fix

--- examples/index.js
+++ examples/index.js
@@ -3,35 +3,22 @@ import CityPicker from '@';
 import '../picker.css';
 
 var picker = new CityPicker({
-  // inputId: 'city-input',
   data: city, // 符合格式的数组
   cancel: function () {
-      console.log('取消城市选择');
+    console.log('取消城市选择');
   },
-  // beforeShow: function () {
-  //     console.log('beforeSuccess回调触发了')
-  //     this.forbidSelect(true)
-  // },
   initialOption: ['辽宁', '大连', '中山区'],
-  success: function(arr) {
-      console.log(arr);
-      var arr2 = [];
-      arr.forEach(function(val) {
-          arr2.push(val.value);
-      });
-      document.getElementById('city-input' + this.pickerNumber).innerHTML = arr2;
+  success: function (arr) {
+    console.log(arr);
+    var arr2 = [];
+    arr.forEach(function (val) {
+      arr2.push(val.value);
+    });
+    document.getElementById('city-input' + this.pickerNumber).innerHTML = arr2;
   }
 });
 
-window.forbid = function () {
-  picker.forbidSelect(true);
-}
-
-window.noForbid = function () {
-  picker.forbidSelect(false);
-}
-
-window.setInitailOption = function  () {
+window.setInitailOption = function () {
   picker.setInitailOption(['湖北', '荆州', '荆州区'])
 }
 
